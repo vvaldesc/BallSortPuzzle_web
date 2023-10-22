46d4096c88ef4916b06a77c60082962f6a9e5204
victorvaldescobos@gmail.com <daw_victorvaldes@riberadeltajo.es>
2023-10-22 13:07:12 +0200

    Arreglo detalles de bolas

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let visor__main=document.getElementById("visor__main")
 let MAXfrascos=4;
 let MAXbolas=4;
+let segundosIniciales=60;
 
 
 /*const audioFondo = () => {
@@ -12,21 +13,27 @@ let MAXbolas=4;
 
 
 const iniciaEntorno = (e) => {
+
     let frascos=document.createDocumentFragment();
+
     for (let index = 0; index < MAXfrascos; index++) {
         let frasco=document.createElement("DIV")
         frasco.classList.add("visor__frasco")
         let bolas=document.createDocumentFragment();
+
         for (let index = 0; index < MAXbolas; index++) {
             let bola=document.createElement("DIV")
             bola.classList.add("pixel-ball")
             bola.innerText=Math.floor(Math.random()*MAXfrascos);
             bolas.append(bola)
+
         }
         frasco.append(bolas)
         frascos.append(frasco)
+
     }
     visor__main.append(frascos)
+    
 }
 
 
