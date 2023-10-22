3042c40e9a5ea714759e458c38f43f9053cb4b5e
victorvaldescobos@gmail.com <daw_victorvaldes@riberadeltajo.es>
2023-10-22 12:40:45 +0200

    Añado frascos y bolas

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+let visor__main=document.getElementById("visor__main")
+let MAXfrascos=4;
+let MAXbolas=4;
+
 
 /*const audioFondo = () => {
     //debugger
@@ -7,6 +11,23 @@
 }*/
 
 
+const iniciaEntorno = (e) => {
+    let frascos=document.createDocumentFragment();
+    for (let index = 0; index < MAXfrascos; index++) {
+        let frasco=document.createElement("DIV")
+        frasco.classList.add("visor__frasco")
+        let bolas=document.createDocumentFragment();
+        for (let index = 0; index < MAXbolas; index++) {
+            let bola=document.createElement("DIV")
+            bola.innerText=Math.random()*MAXfrascos;
+            bolas.append(bola)
+        }
+        bolas.append(frasco)
+        frascos.append(frasco)
+    }
+    visor__main.append(frascos)
+}
 
 
+visor__main.addEventListener("click",iniciaEntorno)
 //document.addEventListener("DOMContentLoaded",audioFondo)
