e69d43f566b312c0c8d3fc7444f73e6fe3380ed7
victorvaldescobos@gmail.com <daw_victorvaldes@riberadeltajo.es>
2023-10-22 12:13:36 +0200

    Audio de fondo provisional volume=0.2

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 
-const audioFondo = () => {
+/*const audioFondo = () => {
     //debugger
     let audio = new Audio('./assets/audio/backgroundAudio.mp3');
-    AudioContext.volume = 0.2;
+    audio.volume = 0.2;
     audio.play();
-}
+}*/
 
 
 
 
-document.addEventListener("DOMContentLoaded",audioFondo)
+//document.addEventListener("DOMContentLoaded",audioFondo)
